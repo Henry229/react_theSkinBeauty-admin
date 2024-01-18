393a138c161455a06a36d4f2ff901591dfcff851
Henry <chunhyungjoon@gmail.com>
2024-01-18 20:44:04 +1100

    fixing configure in oracle server

--- backend/server.js
+++ backend/server.js
@@ -16,34 +16,6 @@ app.use(categoryRoute);
 app.use(serviceRoute);
 app.use(bookRoute);
 
-// const mysql = require('mysql');
-// const cors = require('cors');
-
-// app.use(cors());
-// app.use(express.json());
-
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'your_username',
-//   password: 'your_password',
-//   database: 'your_database_name',
-// });
-
-// app.post('/add-customer', (req, res) => {
-//   const { firstName, lastName, mobile, email } = req.body;
-//   const query =
-//     'INSERT INTO customers (firstName, lastName, mobile, email) VALUES (?, ?, ?, ?)';
-
-//   db.query(query, [firstName, lastName, mobile, email], (err, result) => {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).send('Error while inserting customer data');
-//     } else {
-//       res.status(201).send('Customer added successfully');
-//     }
-//   });
-// });
-
 const PORT = process.env.APP_PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
