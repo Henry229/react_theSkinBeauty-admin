0b622c5b92bb54f5e8e61bf0144255531c8fe26d
Henry <chunhyungjoon@gmail.com>
2024-01-22 22:42:07 +1100

    fixed exit-all.sh for path of backend6

--- scripts/exit-all.sh
+++ scripts/exit-all.sh
@@ -1,3 +1,3 @@
 cd /home/ubuntu/react_theSkinBeauty-admin/backend
 
-docker-compose -f docker-compose.yml --env-file config/.server.env down && npx pm2 delete all
+docker-compose -f docker-compose.yml --env-file ../config/.server.env down && npx pm2 delete all
