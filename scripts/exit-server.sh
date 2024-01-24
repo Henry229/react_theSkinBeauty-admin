c8d8e213ec1659233531064f91923ada96c7311e
Henry <chunhyungjoon@gmail.com>
2024-01-25 08:26:50 +1100

    fixing start-all.sh & exit-server.sh 2

--- scripts/exit-server.sh
+++ scripts/exit-server.sh
@@ -1,2 +1,2 @@
-docker-compose -f /home/ubuntu/react_theSkinBeauty-admin/backend/docker-compose.yml --env-file /home/ubuntu/react_theSkinBeauty-admin/config/.server.env down
-###
+# docker-compose -f /home/ubuntu/react_theSkinBeauty-admin/backend/docker-compose.yml --env-file /home/ubuntu/react_theSkinBeauty-admin/config/.server.env down && pm2 delete all && echo "All services have been stopped."
+cd /home/ubuntu/react_theSkinBeauty-admin/backend && docker-compose -f docker-compose.yml --env-file ../config/.server.env down && pm2 delete all && echo "All services have been stopped."
