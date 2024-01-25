fca7274eacfbdc68f29b1c3ad21641e23826a29d
Henry <chunhyungjoon@gmail.com>
2024-01-25 11:07:42 +1100

    added dockerfile

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -8,10 +8,11 @@ CHECK_OS="`uname -s`"
 if [[ ${CHECK_OS} = "Darwin"* ]]; then
   npm run build --prefix ./server/app && docker-compose -f ./server/docker-compose.yml --env-file ./config/.server.env up -d && npm run start:client
 elif [[ "$CHECK_OS" = "Linux"* ]]; then
-  pm2 start /home/ubuntu/react_theSkinBeauty-admin/backend/server.js --name backend-theSkinBeauty && \
-  docker-compose -f /home/ubuntu/react_theSkinBeauty-admin/backend/docker-compose.yml --env-file /home/ubuntu/react_theSkinBeauty-admin/config/.server.env up -d && \
-  npm run start:client && \
-  echo "All services have been started."
+  # pm2 start /home/ubuntu/react_theSkinBeauty-admin/backend/server.js --name backend-theSkinBeauty && \
+  # docker-compose -f /home/ubuntu/react_theSkinBeauty-admin/backend/docker-compose.yml --env-file /home/ubuntu/react_theSkinBeauty-admin/config/.server.env up -d && \
+  # npm run start:client && \
+  # echo "All services have been started."
+  npm run build --prefix ./backend && docker-compose -f ./backend/docker-compose.yml --env-file ./config/.server.env up -d && npm run start:client
   # npm start --prefix ./backend && docker-compose -f ./backend/docker-compose.yml --env-file ./config/.server.env up -d && npm run start:client
 elif [[ ${CHECK_OS} = "MINGW32"* ]]; then
   npm run start:windows
